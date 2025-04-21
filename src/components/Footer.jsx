7e7ee3fce92f638e07120175be103c10015c9e57
Robin-Roy11 <robinroy.ax@gmail.com>
2025-04-21 13:46:37 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+      <footer className="footer">
+        <div className="container">
+          <p>© {new Date().getFullYear()} MovieFinder. Built with 💙 by Robin.</p>
+        </div>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
